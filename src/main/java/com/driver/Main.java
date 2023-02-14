b98229ba91a0a50e891cdd1becf51cf4c029628a
Hariomsingh98 <hariomjawadi100@gmail.com>
2023-02-14 22:22:25 +0530

    changes made in main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,6 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     DeluxePizza dp = new DeluxePizza(true);
-   // System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
     dp.addTakeaway();
@@ -12,7 +11,6 @@ public class Main {
     Pizza p = new Pizza(false);
     p.addExtraCheese();
     p.addExtraToppings();
-   // p.addTakeaway();
     System.out.println(p.getBill());
   }
 }
